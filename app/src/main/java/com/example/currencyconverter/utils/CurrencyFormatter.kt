d1a8f940f1e4623cedc8efd5463594954f1a8b81
Asim Khatri <101165277+asimkhatri90@users.noreply.github.com>
2024-04-20 20:10:27 +0500

    Fixing currency format case for amounts less than thousand

--- app/src/main/java/com/example/currencyconverter/utils/CurrencyFormatter.kt
+++ app/src/main/java/com/example/currencyconverter/utils/CurrencyFormatter.kt
@@ -4,20 +4,21 @@ import java.util.Locale
 
 object CurrencyFormatter {
 
-    fun format(number: Double): String {
+    /**
+     * Convert the amount into string representation of thousands, millions, billions and trillions
+     */
+    fun format(amount: Double): String {
         val thousand = 1_000L
         val million = 1_000_000L
         val billion = 1_000_000_000L
         val trillion = 1_000_000_000_000L
 
-        val amount = when {
-            number >= trillion -> "%.2f T".format(Locale.US, number / trillion)
-            number >= billion -> "%.2f B".format(Locale.US, number / billion)
-            number >= million -> "%.2f M".format(Locale.US, number / million)
-            number >= thousand -> "%.2f K".format(Locale.US, number / thousand)
-            else -> number.toString()
+        return when {
+            amount >= trillion -> "%.2f T".format(Locale.US, amount / trillion)
+            amount >= billion -> "%.2f B".format(Locale.US, amount / billion)
+            amount >= million -> "%.2f M".format(Locale.US, amount / million)
+            amount >= thousand -> "%.2f K".format(Locale.US, amount / thousand)
+            else -> "%.3f".format(Locale.US, amount)
         }
-//        "%.2f B".format(Locale.US, number / billion)
-        return amount
     }
 }
