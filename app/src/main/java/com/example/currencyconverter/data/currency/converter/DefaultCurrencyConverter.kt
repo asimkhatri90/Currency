de93f450f1663da91f5513a77116996ff61d0c25
Asim Khatri <101165277+asimkhatri90@users.noreply.github.com>
2024-04-20 20:01:56 +0500

    Adding more fixes related to UI

--- app/src/main/java/com/example/currencyconverter/data/currency/converter/DefaultCurrencyConverter.kt
+++ app/src/main/java/com/example/currencyconverter/data/currency/converter/DefaultCurrencyConverter.kt
@@ -6,6 +6,6 @@ class DefaultCurrencyConverter : CurrencyConverter {
 
     override fun convert(amount: Double, fromCurrency: Currency, toCurrency: Currency): Double {
         val conversionFactor = toCurrency.rate / fromCurrency.rate
-        return String.format("%.4f", (amount * conversionFactor)).toDouble()
+        return (amount * conversionFactor)
     }
 }
